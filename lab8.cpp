b04905f570b31c0820a01181f46b0091ec6d21ac
Jan-Tomaszyk <jantomaszyk@gmail.com>
2026-02-01 03:52:09 +0100

    z1-2cpp

--- lab8.cpp
+++ lab8.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <mutex>
+
 /*
 #include <sstream>
 #include <list>
@@ -13,28 +15,84 @@
 using namespace std;
 
 //z1
-class DatabaseConnection {
-
+class DatabaseConnection
+{
+private:
+    static DatabaseConnection * instance;
+    //static std::mutex mutex_;
+protected:
+   DatabaseConnection(string value){value_=value;}
+   ~DatabaseConnection() {}
+   string value_;
 public:
-    static DatabaseConnection* getInstance() {}
+    DatabaseConnection(DatabaseConnection &other) = delete;
+    void operator=(const DatabaseConnection &) = delete;
 
+    static DatabaseConnection* getInstance(string value)
+    {
+        if (!instance)
+        {
+            instance = new DatabaseConnection(value);
+        };
+        return instance;
+    }
+    string value() const
+    {
+        return value_;
+    }
 };
 
 //z2
-class Vehicle {
+class Vehicle
+{
+protected:
+    string vehicleType;
+public:
+    Vehicle(string vehicleType) : vehicleType(vehicleType){}
+    inline virtual void getVehicleType() const=0;
+    enum VehicleType
+    {
+        Car,
+        Bike
+    };
+};
+
+class Car : public Vehicle
+{
+public:
+    Car() : Vehicle("Car"){}
+    inline virtual void getVehicleType() const
+    {
+        cout<<vehicleType<<endl;
+    }
 
 };
 
-class Car : public Vehicle {
-
+class Bike : public Vehicle
+{
+public:
+    Bike() : Vehicle("Bike"){}
+    inline virtual void getVehicleType() const
+    {
+        cout<<vehicleType<<endl;
+    }
 };
 
-class Bike : public Vehicle {
-
-};
-
-class VehicleFactory {
-
+class VehicleFactory
+{
+public:
+    static Vehicle* createVehicle(const Vehicle::VehicleType tt)
+    {
+        switch(tt)
+        {
+            case Vehicle::VehicleType::Car:
+                std::cout<<"Buduje samochód!"<<std::endl;
+                return new Car();
+            case Vehicle::VehicleType::Bike:
+                std::cout<<"Buduje Rower!"<<std::endl;
+                return new Bike();
+        }
+    }
 };
 
 //z3
@@ -69,21 +127,33 @@ public:
 
 //z4
 
-
+DatabaseConnection* DatabaseConnection::instance = nullptr;
 
 void z1()
 {
+    DatabaseConnection* connection1 = DatabaseConnection::getInstance("FOO");
+    DatabaseConnection* connection2 = DatabaseConnection::getInstance("BAR");
     cout << "Czy obie instancje są takie same? " << (connection1 == connection2) << endl;
 }
 
 void z2()
 {
-    cout << "Czy obie instancje są takie same? " << (connection1 == connection2) << endl;
+    Vehicle* werT34 = VehicleFactory::createVehicle(Vehicle::VehicleType::Bike);
+    
+    cout<<"Wybudowano rower: ";
+    werT34->getVehicleType();
+
+    Vehicle* brama = VehicleFactory::createVehicle(Vehicle::VehicleType::Car);
+
+    cout<<"Wybudowano auto: ";
+    brama->getVehicleType();
+
+    //cin.get();
 }
 
-void z1()
+void z3()
 {
-    cout << "Czy obie instancje są takie same? " << (connection1 == connection2) << endl;
+
 }
 
 int main()
@@ -92,14 +162,18 @@ int main()
     while (kont == "T" || kont == "t")
     {
         int odp;
-        cout << "Wybierz zestaw zadań do aktywacji";
+        cout << "Wybierz zadanie do aktywacji(1-3)";
         cin >> odp;
         switch (odp)
         {
         case 1:
-            c
+            z1();
+            break;
+        case 2:
+            z2();
             break;
         default:
+            z3();
             break;
         }
         cout << "Czy chcesz kontynuacji?(T/N)";
---
+++ lab8.java
@@ -0,0 +1,19 @@
+class DatabaseConnection {
+    
+
+    private DatabaseConnection() {
+        
+    }
+
+    public static DatabaseConnection getInstance() {
+     
+    }
+
+    public void executeQuery(String query) {
+       
+    }
+}
+
+public class Main {
+    
+}
