63bedf46fbdd5702c2bfb1bd7a6dffee150b72c2
Jan-Tomaszyk <jantomaszyk@gmail.com>
2026-02-01 21:37:11 +0100

    z1java

--- lab8.java
+++ lab8.java
@@ -1,18 +1,26 @@
 
 //z1
-class DatabaseConnection {
-    
+class DatabaseConnection
+{
+    private static DatabaseConnection single_instance = null;
+    public String s;
 
-    private DatabaseConnection() {
-        
+    private DatabaseConnection()
+    {
+        s = "Hello I am a string part of Singleton class";
     }
 
-    public static DatabaseConnection getInstance() {
-     
+    public static /*synchronized*/ DatabaseConnection getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new Singleton();
+
+        return single_instance;
     }
 
-    public void executeQuery(String query) {
-       
+    public void executeQuery(String query)
+    {
+       println("executing query");
     }
 }
 
@@ -69,8 +77,50 @@ public class Main {
     {
         
     }*/
-    public static void main(String[] args) {
+    public static void z1()
+    {
+        Singleton x = Singleton.getInstance();
 
+        // Instantiating Singleton class with variable y
+        Singleton y = Singleton.getInstance();
+
+        // Instantiating Singleton class with variable z
+        Singleton z = Singleton.getInstance();
+
+        // Printing the hash code for above variable as
+        // declared
+        System.out.println("Hashcode of x is "
+                           + x.hashCode());
+        System.out.println("Hashcode of y is "
+                           + y.hashCode());
+        System.out.println("Hashcode of z is "
+                           + z.hashCode());
+
+        // Condition check
+        if (x == y && y == z) {
+
+            System.out.println(
+                "Three objects point to the same memory location on the heap i.e, to the same object");
+        }
+
+        else {
+            
+            System.out.println(
+                "Three objects DO NOT point to the same memory location on the heap");
+        }
+    }
+    public static void z2()
+    {
+
+    }
+    public static void z3()
+    {
         System.out.println(pizza);
+    }
+    public static void main(String[] args)
+    {
+        z1();
+        z2();
+        z3();        
     }
 }
